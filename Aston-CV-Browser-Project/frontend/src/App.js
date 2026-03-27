5cd296b40d9a4cfd8ea927131c703965d22023c0
Gabriel <deng93003@gmail.com>
2026-03-27 00:19:25 +0000

    Add Three.js animated background and fix box shadows

--- Aston-CV-Browser-Project/frontend/src/App.js
+++ Aston-CV-Browser-Project/frontend/src/App.js
@@ -5,22 +5,27 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Navbar from "./components/Navbar";
+import ThreeBackground from "./components/ThreeBackground";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="relative min-h-screen overflow-x-hidden">
-        <Navbar />
+      <div className="app-shell">
+        <ThreeBackground />
+        <div className="app-content">
+          <Navbar />
 
-        <main className="mx-auto w-full max-w-5xl px-4 pb-16 pt-8 sm:px-6 lg:px-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cv/:id" element={<CVDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </main>
+          <main className="mx-auto w-full max-w-5xl px-4 pb-16 pt-8 sm:px-6 lg:px-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cv/:id" element={<CVDetails />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
       </div>
     </Router>
   );
